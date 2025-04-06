031da6bcab8ad86f426b5e28cc266115983d68e2
Jiayu Ye <106568221+yjy-97@users.noreply.github.com>
2025-04-06 16:58:56 +0800

    Update modeling.py

--- models/modeling.py
+++ models/modeling.py
@@ -459,10 +459,6 @@ class CmdVIT(nn.Module):
 
 CONFIGS = {
     'ViT-B_16': configs.get_b16_config(),
-    'ViT-B_32': configs.get_b32_config(),
-    'ViT-L_16': configs.get_l16_config(),
-    'ViT-L_32': configs.get_l32_config(),
-    'ViT-H_14': configs.get_h14_config(),
     'R50-ViT-B_16': configs.get_r50_b16_config(),
     'testing': configs.get_testing(),
 }
